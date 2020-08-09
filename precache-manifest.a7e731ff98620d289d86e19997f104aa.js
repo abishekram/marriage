cfda7b17e54677b374635c4eed3c5d7c7e5ec78d
Abishek ram <AbishekRam.Ramachandran@softwareag.com>
2020-08-09 21:49:44 +0530

    Draft 1

---
+++ precache-manifest.a7e731ff98620d289d86e19997f104aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbbe47a7804fcee9d627bc9bb3e9744",
+    "url": "/marriage/index.html"
+  },
+  {
+    "revision": "7b967c518797c09bd706",
+    "url": "/marriage/static/css/main.2197ee9e.chunk.css"
+  },
+  {
+    "revision": "0b6392895a7124571913",
+    "url": "/marriage/static/js/2.fe379439.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marriage/static/js/2.fe379439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b967c518797c09bd706",
+    "url": "/marriage/static/js/main.78a4ddbd.chunk.js"
+  },
+  {
+    "revision": "e31d99ca8b911794ddd9",
+    "url": "/marriage/static/js/runtime-main.395c0253.js"
+  },
+  {
+    "revision": "1ab76c02b9a66222590e73ba39d519af",
+    "url": "/marriage/static/media/arrow.1ab76c02.svg"
+  },
+  {
+    "revision": "d2236617f41f638c0ae9945d8d0f7ed3",
+    "url": "/marriage/static/media/background_color.d2236617.png"
+  },
+  {
+    "revision": "08020900f28322fb19b6bff22ac1f85a",
+    "url": "/marriage/static/media/background_top_1.08020900.png"
+  },
+  {
+    "revision": "eeb68ba00e758b6b7a0102a2a3142190",
+    "url": "/marriage/static/media/cake.eeb68ba0.svg"
+  },
+  {
+    "revision": "2085a9f50c9f34d0ce611ff543758b51",
+    "url": "/marriage/static/media/heart.2085a9f5.svg"
+  },
+  {
+    "revision": "7fd622e322f8c36ab94d1ee987e0d36e",
+    "url": "/marriage/static/media/us_paint.7fd622e3.png"
+  }
+]);
